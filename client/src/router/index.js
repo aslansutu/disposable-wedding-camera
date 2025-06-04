dcd71735eeeb4eceb060060ad80c4e896f718650
Atakan Onol <atakan.onol@oruba.com.tr>
2025-06-04 18:16:50 +0300

    Seperated Camera into different components

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import CameraView from '../components/CameraView.vue'
+import CameraView from '../views/CameraView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
---
+++ client/src/views/CameraView.vue
@@ -0,0 +1,25 @@
+<template>
+    <CameraLens />
+    <button>
+        CLICK
+    </button>
+</template>
+
+<script setup>
+import CameraLens from '../components/CameraLens.vue'
+</script>
+
+<style scoped>
+button {
+    position: fixed;
+    top: 10px;
+    left: 10px;
+    z-index: 1000;
+    padding: 10px 20px;
+    background-color: #007bff;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+}
+</style>
